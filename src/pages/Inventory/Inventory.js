3609a344174012f894f046f42d727634a57565d7
dipujscoder <mai.dipu2013@gmail.com>
2022-05-12 23:48:10 +0600

    servicess and staff

---
+++ src/pages/Inventory/Inventory.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Filter from '../../components/Filter/Filter';
+import Avatar from '../../components/Avatar';
+
+
+import user from '../../assets/images/avatar.png';
+
+export default function Staff() {
+    return (
+        <>
+            <Filter
+                pageTitle="Invetory"
+            />
+            <div>
+
+            </div>
+        </>
+    )
+}
