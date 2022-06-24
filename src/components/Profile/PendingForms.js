77a5b8bd98b89f1705c11e6425a14ffc5eaeec6c
dipujscoder <mai.dipu2013@gmail.com>
2022-06-24 21:41:07 +0600

    fix: form of cuterm tab content

--- src/components/Profile/PendingForms.js
+++ src/components/Profile/PendingForms.js
@@ -3,120 +3,238 @@ import React from "react";
 export const PendingForms = () => {
   return (
     <>
-      <div className="card card-default">
-        <div className="card-body">
-          <div className="row px-2">
-            <div className="row mt-3">
-              <div className="col-md-6 col-10">
-                <h3 className="text-uppercase">Pending Forms</h3>
-              </div>
-              <div className="col-md-6 col-10">
-                <h6 className="text-end text-warning">View All</h6>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-md-11 col-10">
-              <div className="d-flex align-items-center">
-                <i className="mdi mdi-eye"></i>
-                <h6 className="ms-2">Eyelash Consent Form</h6>
-              </div>
-            </div>
-            <div className="col-md-1 col-2">
-              <button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button>
-            </div>
+      <div className="card card-default mb-5">
+        <div className="card-body px-5">
+
+          <div className="d-flex align-items-center justify-content-between">
+            <h4 className="text-uppercase text-dark">Pending Forms</h4>
+            <button className="btn btn-link text-warning">Edit</button>
           </div>
 
-          <div className="row">
-            <div className="col-md-11 col-10">
-              <div className="d-flex align-items-center">
-                <i className="mdi mdi-face-woman-profile"></i>
-                <h6 className="ms-2">Facial Consent Form</h6>
-              </div>
-            </div>
-            <div className="col-md-1 col-2">
-              <button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button>
-            </div>
+          <div className="table-responsive">
+            <table class="table table-default table-striped-light">
+              <tbody>
+                <tr >
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td >
+                    Eyelash Consent Form
+                  </td>
+                  <td >
+                    <div className="d-flex justify-content-end" ><button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button></div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td>
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    <div className="d-flex justify-content-end" ><button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button></div>
+                  </td>
+                </tr>
+
+
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td>
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    <div className="d-flex justify-content-end" ><button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button></div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td>
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    <div className="d-flex justify-content-end" ><button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button></div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td>
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    <div className="d-flex justify-content-end" ><button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button></div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td>
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    <div className="d-flex justify-content-end" ><button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button></div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td>
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    <div className="d-flex justify-content-end" ><button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button></div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td>
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    <div className="d-flex justify-content-end" ><button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button></div>
+                  </td>
+                </tr>
+
+
+              </tbody>
+            </table>
           </div>
 
-          <div className="row">
-            <div className="col-md-11 col-10">
-              <div className="d-flex align-items-center">
-                <i className="mdi mdi-eye"></i>
-                <h6 className="ms-2">Eyelash Consent Form</h6>
-              </div>
-            </div>
-            <div className="col-md-1 col-2">
-              <button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col-md-11 col-10">
-              <div className="d-flex align-items-center">
-                <i className="mdi mdi-face-woman"></i>
-                <h6 className="ms-2">Facial Consent Form</h6>
-              </div>
-            </div>
-            <div className="col-md-1 col-2">
-              <button className="btn btn-sm mdi mdi-pencil font-size-20 text-end"></button>
-            </div>
-          </div>
         </div>
       </div>
 
-      <div className="card card-default mt-3">
-        <div className="card-body">
-          <div className="row">
-            <div className="col-md-6 col-10">
-              <h3 className="text-uppercase">Completed Forms</h3>
-            </div>
-            <div className="col-md-6 col-10">
-              <h6 className="text-end text-warning">View All</h6>
-            </div>
+      <div className="card card-default">
+        <div className="card-body px-5">
+
+          <div className="d-flex align-items-center justify-content-between">
+            <h4 className="text-uppercase text-dark">Completed Forms</h4>
+            <button className="btn btn-link text-warning">Edit</button>
           </div>
-          <div className="row mt-2">
-            <div className="col-md-11 col-10">
-              <div className="d-flex align-items-center">
-                <i className="mdi mdi-eye"></i>
-                <h6 className="ms-2">Eyelash Consent Form</h6>
-              </div>
-            </div>
-            <div className="col-md-1 col-2">
-              <div className="d-flex align-items-center justify-content-between">
-                <i className="mdi mdi-eye"></i>
-                <i className="mdi mdi-delete-outline"></i>
-              </div>
-            </div>
+
+          <div className="table-responsive">
+            <table class="table table-default table-striped-light">
+              <tbody>
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td >
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    4:30 pm Mon, July 28th, 2020
+                  </td>
+                  <td >
+                    <div className="d-flex justify-content-end" >
+                      <button className="btn btn-sm mdi mdi-eye font-size-20 text-end me-3"></button>
+                      <button className="btn btn-sm mdi mdi-delete font-size-20 text-end"></button>
+                    </div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td >
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    4:30 pm Mon, July 28th, 2020
+                  </td>
+                  <td >
+                    <div className="d-flex justify-content-end" >
+                      <button className="btn btn-sm mdi mdi-eye font-size-20 text-end me-3"></button>
+                      <button className="btn btn-sm mdi mdi-delete font-size-20 text-end"></button>
+                    </div>
+                  </td>
+                </tr>
+
+
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td >
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    4:30 pm Mon, July 28th, 2020
+                  </td>
+                  <td >
+                    <div className="d-flex justify-content-end" >
+                      <button className="btn btn-sm mdi mdi-eye font-size-20 text-end me-3"></button>
+                      <button className="btn btn-sm mdi mdi-delete font-size-20 text-end"></button>
+                    </div>
+                  </td>
+                </tr>
+
+
+                <tr>
+                  <td style={{ width: "50px" }}>
+                    <div className="icon icon-sm rounded-circle bg-light">
+                      <i className='mdi mdi-eye'></i>
+                    </div>
+                  </td>
+                  <td >
+                    Eyelash Consent Form
+                  </td>
+                  <td>
+                    4:30 pm Mon, July 28th, 2020
+                  </td>
+                  <td >
+                    <div className="d-flex justify-content-end" >
+                      <button className="btn btn-sm mdi mdi-eye font-size-20 text-end me-3"></button>
+                      <button className="btn btn-sm mdi mdi-delete font-size-20 text-end"></button>
+                    </div>
+                  </td>
+                </tr>
+
+
+
+
+              </tbody>
+            </table>
           </div>
-          <div className="row mt-2">
-            <div className="col-md-11 col-10">
-              <div className="d-flex align-items-center">
-                <i className="mdi mdi-eye"></i>
-                <h6 className="ms-2">Eyelash Consent Form</h6>
-              </div>
-            </div>
-            <div className="col-md-1 col-2">
-              <div className="d-flex align-items-center justify-content-between">
-                <i className="mdi mdi-eye"></i>
-                <i className="mdi mdi-delete-outline"></i>
-              </div>
-            </div>
-          </div>
-          <div className="row mt-2">
-            <div className="col-md-11 col-10">
-              <div className="d-flex align-items-center">
-                <i className="mdi mdi-eye"></i>
-                <h6 className="ms-2">Eyelash Consent Form</h6>
-              </div>
-            </div>
-            <div className="col-md-1 col-2">
-              <div className="d-flex align-items-center justify-content-between">
-                <i className="mdi mdi-eye"></i>
-                <i className="mdi mdi-delete-outline"></i>
-              </div>
-            </div>
-          </div>
+
         </div>
       </div>
     </>
